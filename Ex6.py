43611541462f14ff92664f9ad108e7e56d0c77ee
EnzoAntunesOliveira <143086296+EnzoAntunesOliveira@users.noreply.github.com>
2023-09-06 10:18:59 -0300

    Update Ex6.py

--- Ex6.py
+++ Ex6.py
@@ -1,4 +1,4 @@
 base = float(input("Digite a base do triangulo"))
 altura = float(input("Digite a altura do triangulo"))
 
-print(f'A área do é triangulo" {base * altura / 2}' )
+print(f'A área do triangulo é {base * altura / 2}' )
